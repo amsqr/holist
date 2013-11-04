6f470b68d9b39051974eb4817ab6f9e727238180
phdowling <philipp.dowling@gmail.com>
2013-11-04 16:10:24 +0100

    cleaned up

--- holist/DatabaseInterface/DatabaseObject.py
+++
@@ -1,7 +0,0 @@
-class DatabaseObject:
-    def toDict(self):
-        raise NotImplementedError("needs to be imlemented!")
-    def __eq__(self, other):
-        return self.sid == other.sid
-    def __hash__(self):
-        return hash(self.sid)
--- holist/TopicModeling/WordType.py
+++
@@ -1,9 +0,0 @@
-from holist.DatabaseInterface import DatabaseObject
-
-class WordType(DatabaseObject.DatabaseObject):
-    def __init__(self, _id, token, frequency):
-        self._id = _id
-        self.sid = token
-        self.frequency = frequency
-    def toDict(self):
-        return {"token":self.sid, "frequency":self.frequency}
