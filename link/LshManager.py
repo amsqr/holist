7886723db485efbf6bc833dee6e350453e06b054
Raoul Elias Friedrich <mail@raoulfriedrich.com>
2014-06-04 11:55:36 +0200

    imported variable from lsa strategy

--- link/LshManager.py
+++ link/LshManager.py
@@ -1,6 +1,7 @@
 __author__ = 'raoulfriedrich'
 
 from lshash import LSHash
+from core.model.semantics.LSA.LSAStrategy import NUM_TOPICS
 
 NUMBER_OF_LSH_INDEXES = 3
 
@@ -11,7 +12,7 @@ class LshManager(object):
         self.lshIndexList = []
 
         for x in xrange(NUMBER_OF_LSH_INDEXES):
-            lsh = LSHash(6, 200)
+            lsh = LSHash(6, NUM_TOPICS)
             self.lshIndexList.append(lsh)
 
 
