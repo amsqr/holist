8e59bb49db9351f08de50a4f3715bbadae577740
Raoul Elias Friedrich <mail@raoulfriedrich.com>
2014-06-04 14:10:14 +0200

    finished lsh manager

--- link/LshManager.py
+++ link/LshManager.py
@@ -1,7 +1,5 @@
 __author__ = 'raoulfriedrich'
 
-import json
-from pprint import pprint
 from lshash import LSHash
 from core.model.semantics.LSA.LSAStrategy import NUM_TOPICS
 
@@ -14,30 +12,47 @@ class LshManager(object):
 
         self.lshIndexList = []
 
+        # create a list of lsh indexes
         for x in xrange(NUMBER_OF_LSH_INDEXES):
             lsh = LSHash(NUMBER_OF_BITS_PER_HASH, NUM_TOPICS)
             self.lshIndexList.append(lsh)
-
-        json_data=open('/Users/raoulfriedrich/Education/TUM/MA2/WebAppEng/Development/legacy/sample_document.bson.json')
-        data = json.load(json_data)
-        pprint(json_data)
-        json_data.close()
-        #addDocument()
 
     # adds a document to all lsh indexes
     def addDocument(self, document):
 
         lsa_vector = document.vectors["LSA"]
 
-        dense = {}
-        for x in range(200):
-            dense[x] = 0
-
-        for dim, val in lsa_vector:
-            dense[dim] = val
-        dense_vector = [value for key, value in dense.items()]
+        dense_vector = self._sparseToDenseConverter(lsa_vector)
 
         database_id = document._id
 
         for x in self.lshIndexList:
             x.index(dense_vector, extra_data=database_id)
+
+    # takes a document and returns database ids of similar documents
+    # uses cosine function to determine similarity
+    def getSimilarDocuments(self, document):
+
+        lsa_vector = document.vectors["LSA"]
+
+        dense_vector = self._sparseToDenseConverter(lsa_vector)
+
+        resultSet = set()
+
+        for x in self.lshIndexList:
+
+            for result in x.query(dense_vector, num_results=10, distance_func="cosine"):
+                resultSet.add(result[0][1])
+
+        return list(resultSet)
+
+    # converts a vector in sparse format to a vector in dense format
+    def _sparseToDenseConverter(self, sparseVector):
+
+        dense = {}
+        for x in range(NUM_TOPICS):
+            dense[x] = 0
+
+        for dim, val in sparseVector:
+            dense[dim] = val
+        return [value for key, value in dense.items()]
