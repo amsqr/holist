f976238bb0723a24507c2a111c87ad61e7c9a997
phdowling <philipp.dowling@gmail.com>
2014-06-11 14:32:53 +0200

    string int concatenation error

--- rebuildLSH.py
+++ rebuildLSH.py
@@ -2,5 +2,5 @@ __author__ = 'dowling'
 import requests
 from core.util import config
 print "requesting LSH index rebuild."
-r = requests.get("http://localhost:" + config.link_node_control_port + "/command?command=rebuild")
+r = requests.get("http://localhost:" + str(config.link_node_control_port) + "/command?command=rebuild")
 print "got: %s, %s" % (r.status_code, r.text)
