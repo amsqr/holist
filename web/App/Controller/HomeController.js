f21363623b9006c4a03af276ad3104133cd4d5b7
sfakir <simon@fakir-it.de>
2014-06-11 19:42:01 +0200

    links active state

--- web/App/Controller/HomeController.js
+++ web/App/Controller/HomeController.js
@@ -1,6 +1,6 @@
 angular.
 module('App')
-    .controller('HomeController', function($scope, $stateParams, $state, GraphService) {
+    .controller('HomeController', function($scope, $stateParams,$log, $http,$state,AppSettings, GraphService) {
         if ($stateParams.query) {
             $scope.searchKeyword = $scope.currentSearch = $stateParams.query;
         } else {
@@ -11,6 +11,22 @@ module('App')
         //
         $scope.searchResults = ["LinkedIn", "google", "Microsoft", "Holist", "Uber", "Sunil Jagani - Person President and Chief Technology Officer @ AllianceTek", "Smartface Inc.", "Facebook", "Vuclip", "Roundforest LTD", "TransferWise"];
 
+        $scope.updateAutocomplete = function(text){
+            //if (text.length < 4) return;
+            var deferred = $.Deferred();
+
+            var url = AppSettings.apiUrl +  'complete_search?entityName=' + text
+            $http({method: 'GET', url: url})
+                .success(function(results) {
+                    console.log(results);
+                    $scope.searchResults = results;
+                    deferred.resolve($scope.searchResults);
+                })
+                .error(function(data, status, headers, config) {
+                    $log.error('error on fetching autocomplete', url);
+                });
+            return deferred.promise();
+        };
 
         //
         // perform search
