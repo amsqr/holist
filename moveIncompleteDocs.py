ba81a19cd9bd9a980114e166bbd179d08a4253ca
phdowling <philipp.dowling@gmail.com>
2014-06-28 12:25:27 +0200

    started data completion script

--- moveIncompleteDocs.py
+++ moveIncompleteDocs.py
@@ -10,5 +10,8 @@ for article in client.holist.articles.find():
 
     if not "named_entities" in article["vectors"] or not "LSA" in article["vectors"]:
         count += 1
+        client.holist.new_document.insert(article)
+        client.holist.articles.remove(article)
 
-print count
+
+print "moved %s articles." % count
