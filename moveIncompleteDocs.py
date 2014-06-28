736a15056df84408d6a99a0b4103f8ca0bfe4331
phdowling <philipp.dowling@gmail.com>
2014-06-28 12:24:11 +0200

    started data completion script

--- moveIncompleteDocs.py
+++ moveIncompleteDocs.py
@@ -7,10 +7,8 @@ from core.util.util import *
 count = 0
 client = getDatabaseConnection()
 for article in client.holist.articles.find():
-    print article["vectors"]
-    break
 
-    if not "NamedEntities" in article["vectors"] or not "LSA" in article["vectors"]:
+    if not "named_entities" in article["vectors"] or not "LSA" in article["vectors"]:
         count += 1
 
 print count
