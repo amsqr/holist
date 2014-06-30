a7c83740f3b7632cdbadc2a8b803e65464e37abf
phdowling <philipp.dowling@gmail.com>
2014-06-30 12:08:58 +0200

    undo robert's changes

--- holist-analysis/relabelLSA.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'dowling'
-import requests
-from core.util import config
-print "requesting LSA relabel of all documents."
-r = requests.get("http://localhost:" + str(config.core_control_port) + "/command?command=relabelLSA")
-print "got: %s, %s" % (r.status_code, r.text)
